5d01c733d3ad3755c3be9d63fc05251af2506489
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-06-11 16:20:30 -0700

    change sign up root

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Scheduler::Application.routes.draw do
   get "meetings/edit"
   get "meetings", to: 'meetings#index', as: 'meetings'
   get "meetings/:id", to: 'meetings#show', as: 'meeting'
-  get "sign_up" => "sessions#new", :as => "sign_up"
+  get "sign_up" => "users#new", :as => "sign_up"
 
   get "execs/new"
   get "execs/edit"
