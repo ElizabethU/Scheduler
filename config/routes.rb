983806a8a2b9ec0c0d30a31e693e8e6c836e92c1
Brooks Kindle <Brooks.Kindle@emc.com>
2014-06-11 15:44:51 -0700

    modify session controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Scheduler::Application.routes.draw do
 
   root "welcome#home"
   resources :users
+  resources :sessions
   get "meetings/new"
   get "meetings/edit"
   get "meetings", to: 'meetings#index', as: 'meetings'
   get "meetings/show"
+  get "sign_up" => "sessions#new", :as => "sign_up"
 
   get "execs/new"
   get "execs/edit"
