85409613beece8bc0e288ad14bbf8d0a07aa25b6
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-06-11 16:14:51 -0700

    application controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      reset_session
+      redirect_to welcome_path, notice: "User not found. Sign in please!"
+  end
+
+  helper_method :current_user
+
+  def check_for_sign_in
+    if current_user == nil
+      redirect_to sign_in_path
+      flash[:notice] = "You need to sign in to do that."
+    end
+  end
 end
