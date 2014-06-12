39f703cd49ebd144226b25d43b29e504d9f3a4dc
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-06-12 11:36:49 -0700

    new meeting form, better meeting show page

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,10 +1,23 @@
 class MeetingsController < ApplicationController
   before_action :set_meeting, only: [:show, :edit, :update, :destroy]
 
+  def whoops
+    render :new
+    flash[:notice] = "There was a problem saving this meeting."
+  end
+
   def new
+    @meeting = Meeting.new
   end
 
   def create
+    @meeting = Event.new(event_params)
+    if @meeting.save
+      redirect_to meeting_path
+      flash[:notice] = "You've created a new meeting!"
+    else
+      whoops
+    end
   end
 
   def edit
@@ -20,7 +33,13 @@ class MeetingsController < ApplicationController
   def show
   end
 
+  private
+
   def set_meeting
     @meeting = Meeting.find(params[:id])
+  end
+
+  def meeting_params
+    params.require(:meeting).permit(:exec_id, :location, :user_id, :start, :duration)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Scheduler::Application.routes.draw do
   get "meetings/:id", to: 'meetings#show', as: 'meeting'
   get "sign_up" => "users#new", :as => "sign_up"
 
-  get "execs/new"
+  get "execs/new", to: 'execs#new', as: 'new_exec'
   get "execs/edit"
   get "execs/show"
   get "execs" => "execs#index"
