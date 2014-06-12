ac7233263ba8252800e69aa0943d80b7d65e22cb
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-06-12 16:49:46 -0700

    flash notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,17 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:email])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			flash[:notice] = "You have successfully signed in"
+			flash[:notice] = "You have successfully signed in!"
 			redirect_to meetings_path
 		else
-			flash[:notice] = "Invalid email/password"
+			flash[:notice] = "Invalid email/password. Please try again."
 			redirect_to root_path
 		end
 	end
 
 	def destroy
 		session[:user_id] = nil
+		flash[:notice] = "You have successfully logged out!"
 		redirect_to root_path
 	end
 
