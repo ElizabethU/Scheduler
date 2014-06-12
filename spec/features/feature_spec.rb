9aca83baa27fe58de2f22425c1c1d6663339a4fa
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-06-12 11:59:45 -0700

    log in feature tests pass

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'home page navigation' do
   let!(:user)    { create :user }
 
-  it 'works' do
+  it 'works', js: true do
     visit "/"
     click_link "Log In"
-    fill_in "Email Address", with: user.email
+    fill_in "email", with: user.email
     fill_in "Password", with: "MyString"
     click_button "Log In"
     expect(current_path).to eq "/"
