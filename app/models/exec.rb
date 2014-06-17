5193d9ef01911ac11b9ae5e6349a62d567fb71ba
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-06-17 01:28:08 -0700

    more cleanup/formatting

--- app/models/exec.rb
+++ app/models/exec.rb
@@ -7,11 +7,11 @@ class Exec < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
 
   def self.alphabetize_array
-    self.alphabetize.map {|exec| [exec[:name], exec[:id]]}
+    self.alphabetize.map { |exec| [exec[:name], exec[:id]] }
   end
 
   def self.alphabetize
-    self.all.sort_by{ |exec| exec.name }
+    self.all.sort_by { |exec| exec.name }
   end
 
 end
