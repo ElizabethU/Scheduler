2a434aac8e0185286ebcca5b7c6376849b2c8ee7
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-06-16 22:56:56 -0700

    cleanup, add @meetings to home action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,15 +17,14 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = 'User was successfully created.'
       redirect_to root_path
     else
+      flash[:notice] = 'There was a problem creating your account!'
       render :new
     end
-
   end
 
   def update
